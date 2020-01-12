9f283f6712cf6d2d30341ebc0bb9ab28ba1a6c88
zsw <1355796120@qq.com>
2020-01-12 10:38:09 +0800

    server1.0

---
+++ src/test/java/com/ag/ServerTests.java
@@ -0,0 +1,13 @@
+package com.ag;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
